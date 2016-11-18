1130a7d3df4d860a39f696a8fd4eae4ca6c6c0ce
Martin Crappe <martin.crappe@gmail.com>
2016-11-18 01:02:01 +0100

    ajout de plusieurs personnes dans la list-gestion erreur

--- phpform/view/form_detail.php
+++ phpform/view/form_detail.php
@@ -2,6 +2,8 @@
 <?php
 session_start();
 require  '../model/tickets.php';
+require  '../model/person.php';
+
 if(isset($_SESSION["reservationModel"])){ //$session could be replace by an access to an DB
     $localTicket = unserialize($_SESSION["reservationModel"]);
     //the object is stored as a string in $SESSSION
@@ -20,6 +22,7 @@ if(isset($_SESSION["reservationModel"])){ //$session could be replace by an acce
         <div class="header">
 
             <h3>DETAILS DES RESERVATIONS</h3>
+            <p style="color:red;"><?php  print($localTicket->getError()); ?></p>
 
 
         </div>
@@ -34,17 +37,37 @@ if(isset($_SESSION["reservationModel"])){ //$session could be replace by an acce
 
           <form method="post" action="../controller/addDetailController.php">
             <input type="hidden" name="reservation"/>
-            <?php for ($i = 0; $i < $localTicket->getNbPlace(); ++$i): ?>
-              <p>Personne <?php print($i+1); ?> </p>
-              <input type="text" name="name[]" placeholder="Nom"  value="<?php  print($localTicket->getName()); ?>" ><br>
-              <input type="text" name="age[]" placeholder="Age" value="<?php  print($localTicket->getAge()); ?>" >
-                <div class="sep"></div>
 
-            <?php endfor; ?>
+
+
+            <?php for ($i = 1; $i <= $localTicket->getNbPlace(); $i++){ ?>
+
+           
+
+            <?php  
+
+              $nameVal = '';
+              $ageVal = '';
+              $personObj = $localTicket->getPeopleIndex($i);
+              if(isset($personObj)){
+                $nameVal = $personObj->getName();
+                $ageVal =  $personObj->getAge();
+              } 
+
+              ?>
+
+              <p>Personne <?php print($i); ?> </p>
+              <input type="text" name="name[<?php print($i); ?>]" placeholder="Nom"  value="<?php print($nameVal); ?> " ><br>
+              <input type="text" name="age[<?php print($i); ?>]" placeholder="Age" value="<?php print($ageVal); ?> " >
+              <div class="sep"></div>
+
+            <?php } ?>
 
             <a><input id="submit" type="submit" class="button" value="Next" ><a/>
           </form>
                       <a href="form_reservation.php" class="button">Précédent</a>
+                                            <a href="cancel_order.php" class="button">Cancel</a>
+
 
 
         </div>

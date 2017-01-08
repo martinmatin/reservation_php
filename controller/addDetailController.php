3ae305f25d19e9500ada5bc1999d95994ee610e9
Martin Crappe <martin.crappe@gmail.com>
2017-01-08 15:02:19 +0100

    final

--- controller/addDetailController.php
+++ controller/addDetailController.php
@@ -11,7 +11,6 @@ if ( isset($_POST['reservation'])  //if I have data from the form
 
 
   foreach ($_POST['name'] as $key=>$name) {
-    //echo "- [Name] $name  [Age] {$_POST['age'][$key]} ; <br />";
     $age = $_POST['age'][$key];
     $person = new Person($name , $age);
     $localTicket->addPeople($person);
@@ -19,7 +18,6 @@ if ( isset($_POST['reservation'])  //if I have data from the form
 
 
   $_SESSION["reservationModel"] = serialize($localTicket); //object to string
-   //header('Location: loadSummaryController.php');
 
 
    $view = new View();
